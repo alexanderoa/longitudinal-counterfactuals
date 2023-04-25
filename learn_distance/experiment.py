f766bd8109cf82a13c6325538d08ea78540372af
Alexander Asemota <alexander.asemota@dorothy.Berkeley.EDU>
2023-04-25 10:28:39 -0700

    Added eicu preprocessing steps

--- learn_distance/experiment.py
+++ learn_distance/experiment.py
@@ -8,6 +8,7 @@ from sklearn.ensemble import RandomForestClassifier
 import scipy as sp
 import sys
 import io
+import argparse
 from tqdm import tqdm, trange
 
 import dice_ml
@@ -18,8 +19,17 @@ import seaborn as sns
 from ordinal import *
 from evaluate import *
 
-problem = 'ARF_4h'
-dataset = 'mimic3'
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--dataset', help='Dataset for experiment', choices=['eicu', 'mimic3'], type=str)
+parser.add_argument('-p', '--problem', help='Prediction problem for experiment', choices=['ARF_4h', 'ARF_12h', 'Shock_4h', 'Shock_12h', 'mortality_48h'], type=str)
+parser.add_argument('-m', '--model', help='sklearn model for experiment', choices=['rf', 'logistic', 'boosting'], type=str)
+parser.add_argument('-l', '--load', action='store_true', help='load preprocessed data')
+
+args = parser.parse_args()
+
+problem = args.problem
+dataset = args.dataset
+model = args.model
 
 s = sparse.load_npz('../data/fiddle/FIDDLE_{dataset}/features/{problem}/s.npz'.format(problem=problem, dataset=dataset)).todense()
 x = sparse.load_npz('../data/fiddle/FIDDLE_{dataset}/features/{problem}/X.npz'.format(problem=problem, dataset=dataset)).todense()
